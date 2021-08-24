c34a0ff889b46f55b758d735fe8d73bbf30c84c6
random9s <jake.august.parham@gmail.com>
2021-08-24 17:31:24 -0500

    update

--- bound/main.go
+++ bound/main.go
@@ -14,48 +14,37 @@ type (
 	philosophers chan *philosopher
 )
 
-func newPhilosopher(diners, thinkers philosophers, left, right fork) *philosopher {
-	var p = &philosopher{left, right}
-	go func() {
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		for phil := range thinkers {
-			time.Sleep(time.Microsecond * time.Duration(r.Int63()))
-			diners <- phil
-		}
-	}()
-	return p
-}
-
-func (p *philosopher) Eat(thinkers philosophers) {
+func (p *philosopher) Eat(r *rand.Rand, diners philosophers) {
 	left := <-p.left
 	right := <-p.right
 
-	time.Sleep(time.Microsecond * 250)
+	time.Sleep(time.Microsecond * 250) //spend some fixed time eating
 
 	p.left <- left
 	p.right <- right
 
-	thinkers <- p
+    time.Sleep(time.Microsecond * time.Duration(r.Int63())) //spend some time thinking
+	diners <- p
 }
 
 func main() {
 	var (
 		numPhilosophers = 5
-		diners          = make(philosophers)                  // diners come as they become hungry and stop thinking
-		forks           = make([]fork, numPhilosophers)       // buffered channel containing all empty seats and all unused fork pairs
-		thinkers        = make(philosophers, numPhilosophers) // buffered channel containing all philosophers in their starting state
+		diners          = make(philosophers) // diners come as they become hungry and stop thinking
+		forks           = []fork{make(fork, 1), make(fork, 1), make(fork, 1), make(fork, 1), make(fork, 1)} // buffered channel containing all empty seats and all unused fork pairs
+        r = rand.New(rand.NewSource(time.Now().UnixNano()))
 	)
 
-	for i := 0; i < 5; i++ {
-		forks[i] = make(fork, 1)
+    for i := 0; i < numPhilosophers; i++ {
 		forks[i] <- struct{}{}
-	}
+    }
 
 	for i := 0; i < numPhilosophers; i++ {
-		thinkers <- newPhilosopher(diners, thinkers, forks[i], forks[(i-1+numPhilosophers)%numPhilosophers]) // initialize our philosophers and start each goroutine
+        time.Sleep(time.Microsecond * time.Duration(r.Int63())) //spend some time thinking
+		diners <- &philosopher{forks[i], forks[(i-1+numPhilosophers)%numPhilosophers]} // initialize our philosophers and start each goroutine
 	}
 
 	for p := range diners {
-		go p.Eat(thinkers)
+		go p.Eat(r, diners)
 	}
 }
